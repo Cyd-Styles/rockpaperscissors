1f7950662191a4832a59066de1508f3ce0f88d89
Cydney Haynes <cydneyc.haynes@gmail.com>
2018-11-27 22:01:02 +0000

    change

--- js/script.js
+++ js/script.js
@@ -4,8 +4,51 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var userChoice = "userChoice"
+var computerChoice = "computerChoice"
+var winner = "winner"
+var randomNumber = "0"
+
+
+ DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function(){
+  $("#userChoice").text($("#input").val());
+  Math.floor(math.Random) * 3);      var computerChoice ["rock", "paper", "scissors"]
+  
+});
 
 
 
-// DOCUMENT READY FUNCTION BELOW
 
+
+if(randomNumber === 0) {
+        $("#computerChoice").html("Rock!"); 
+    }
+else if(randomNumber === 1 ) {
+        $("#computerChoice").html("Paper!"); 
+    }
+else if(randomNumber === 2) {
+        $("#computerChoice").html("Scissors!"); 
+    }
+
+else if(randomNumber === 0) {
+        $("#computerChoice").html("Rock!"); 
+}
+
+else if(randomNumber === 0) {
+        $("#computerChoice").html("Paper!"); 
+}
+else if(randomNumber === 0) {
+        $("#computerChoice").html("Scissors!"); 
+}
+
+if(randomNumber === 0) {
+        $("#computerChoice").html("Rock!"); 
+}
+else if(randomNumber === 0) {
+        $("#computerChoice").html("Paper!"); 
+}
+else if(randomNumber === 0) {
+        $("#computerChoice").html("Scissors!"); 
+    
+}
